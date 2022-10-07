5c891b8588def9b530e60585337b5c721491f582
Jilsa212 <jilsa.chandarana@yahoo.com>
2022-10-07 17:55:45 +0530

    PR

---
+++ timepass.py
@@ -0,0 +1,76 @@
+# How to pass time with Python
+import time
+import random
+import os
+import sys
+import subprocess
+import webbrowser
+import socket
+import requests
+import urllib.request
+import urllib.parse
+import urllib.error
+import json
+from bs4 import BeautifulSoup
+from datetime import datetime
+from datetime import timedelta
+from datetime import date
+from time import sleep
+from random import randint
+from random import choice
+from random import shuffle
+from random import sample
+from random import random
+from random import uniform
+from random import randrange
+from random import triangular
+from random import expovariate
+from random import gammavariate
+from random import gauss
+from random import lognormvariate
+from random import normalvariate
+from random import vonmisesvariate
+from random import paretovariate
+from random import weibullvariate
+from random import choice
+from random import choices
+from random import shuffle
+from random import sample
+from random import random
+from random import uniform
+from random import randrange
+from random import triangular
+from random import expovariate
+from random import gammavariate
+from random import gauss
+from random import lognormvariate
+from random import normalvariate
+from random import vonmisesvariate
+from random import paretovariate
+from random import weibullvariate
+from random import choice
+from random import choices
+from random import shuffle
+from random import sample
+from random import random
+from random import uniform
+from random import randrange
+from random import triangular
+from random import expovariate
+from random import gammavariate
+from random import gauss
+from random import lognormvariate
+from random import normalvariate
+from random import vonmisesvariate
+from random import paretovariate
+from random import weibullvariate
+from random import choice
+from random import choices
+from random import shuffle
+from random import sample
+from random import random
+from random import uniform
+from random import randrange
+from random import triangular
+from random import expovariate
+from random import gammavariate
